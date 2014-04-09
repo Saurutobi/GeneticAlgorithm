565047f42daca2ccde8fc1b9ba1e5795c3f30064
Saurutobi <saurutobi@gmail.com>
2014-04-09 19:09:40 -0400

    made some basic changes

--- ReadMe.txt.txt
+++
@@ -1,2 +0,0 @@
-We're making a program to model a Genetic Algorithm.
-The program will have 2 version: a serial/squential version and a parallel version written in openmp.
---
+++ Sequential/sequential.c
@@ -0,0 +1,27 @@
+//Authors: Marcel Englmaier & Michael Sharp
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#define BITLENGTH 20
+
+struct subject
+{
+	char bits[BITLENGTH];
+	float fitness;
+};
+
+int main(int argc, char *argv[])
+{
+	int populationSize = atoi(argv[1]);
+	printf("Running with:\n    Population Size = %d\n    Bit Length = %d\n", populationSize, BITLENGTH);
+
+	
+	//subject population[populationSize];
+	
+	
+	
+	
+	return 0;
+}
