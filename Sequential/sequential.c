0904d432bb55640be3704da6d15dcfda801b9930
Saurutobi <saurutobi@gmail.com>
2014-04-09 20:06:37 -0400

    we've now got our adam&eve generation all set up

--- Sequential/sequential.c
+++ Sequential/sequential.c
@@ -3,25 +3,51 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <stdlib.h>
 
 #define BITLENGTH 20
 
 struct subject
 {
-	char bits[BITLENGTH];
+	int bits[BITLENGTH];
 	float fitness;
 };
 
 int main(int argc, char *argv[])
 {
+	srand(time(NULL));
 	int populationSize = atoi(argv[1]);
 	printf("Running with:\n    Population Size = %d\n    Bit Length = %d\n", populationSize, BITLENGTH);
-
 	
-	//subject population[populationSize];
+	int i = 0;
+	int j= 0;
+	struct subject sheep[populationSize];
+	
+	for(i = 0; i < populationSize; i++)
+	{
+		for(j = 0; j < BITLENGTH; j++)
+		{
+			sheep[i].bits[j] = rand() %2;
+		}
+		sheep[i].fitness = 0.0f;
+		printf("subject %d bits = ", i);
+		for(j = 0; j < BITLENGTH; j++)
+		{
+			printf("%d", sheep[i].bits[j]);
+		}
+		printf(" with fitness %f\n", i, sheep[i].bits, sheep[i].fitness);
+	}
+	
+	
+	
 	
 	
 	
 	
 	return 0;
 }
+
+
+
+
+
