6001b82c8afd7ac8674253d771c87e144faca756
Saurutobi <saurutobi@gmail.com>
2014-04-12 04:35:09 -0500

    magic is happening. this project is almost done!

--- Sequential/sequential.c
+++ Sequential/sequential.c
@@ -14,13 +14,14 @@ struct subject
 };
 
 float assignFitness(int [], float);
+void makeMeABaby(float, struct subject *, int *);
 
 int main(int argc, char *argv[])
 {
 	srand(time(NULL));
 	double populationSize = atoi(argv[1]);
 	float target = (float)atoi(argv[2]);
-	printf("Running with:\n    Population Size = %d\n    Bit Length = %d\n", populationSize, BITLENGTH);
+	printf("Running with:\n    Population Size = %f\n    Bit Length = %d\n", populationSize, BITLENGTH);
 	
 	int i = 0;
 	int j= 0;
@@ -46,6 +47,8 @@ int main(int argc, char *argv[])
 	
 	int solutionFound = 0;	//0 if false, 1 if true
 	
+	struct subject *fuckSpawn = (struct subject*)malloc(sizeof(struct subject) * populationSize);
+	
 	while(solutionFound != 1)
 	{
 		float totalFitness = 0.0f;
@@ -53,9 +56,9 @@ int main(int argc, char *argv[])
 		for(i = 0; i < populationSize; i++)
 		{
 			sheep[i].fitness = assignFitness(sheep[i].bits, target);
-			//printf("subject %d with fitness %f\n", i, sheep[i].fitness);
+			printf("subject %d with fitness %f\n", i, sheep[i].fitness);
 			totalFitness += sheep[i].fitness;
-			//printf(" Total Fitness%f\n", totalFitness);
+			printf(" Total Fitness%f\n", totalFitness);
 		}
 	
 		for(i = 0; i < populationSize; i++)
@@ -68,23 +71,20 @@ int main(int argc, char *argv[])
 			}
 		}
 		
-		struct subject *fuckSpawn = (struct subject*)malloc(sizeof(struct subject) * populationSize);
-		
 		double fuckSpawnPopulationSize = 0;
+		
+		int* fuckSpawn1Bits = malloc(sizeof(int) * BITLENGTH);
+		int* fuckSpawn2Bits = malloc(sizeof(int) * BITLENGTH);
 		
 		while(fuckSpawnPopulationSize < populationSize)
 		{
-		
+			makeMeABaby(totalFitness, sheep, fuckSpawn1Bits);
+			makeMeABaby(totalFitness, sheep, fuckSpawn2Bits);
+			fuckSpawnPopulationSize++;
 		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
+		free(fuckSpawn1Bits);
+		free(fuckSpawn1Bits);
 		
 		
 		
@@ -107,7 +107,14 @@ float assignFitness(int bits[], float target)
 	return 1.0f;
 }
 
-
+void makeMeABaby(float totalFitness, struct subject *test, int * meh)
+{
+	int i = 0;
+	for(i = 0; i < BITLENGTH; i++)
+	{
+		meh[i] = 1;
+	}
+}
 
 
 
